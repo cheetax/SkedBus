136819f57db6bbc4e1e41ee9d2144f0a5e708cdb
cheetax <dmitriy.grebenev@gmail.com>
2023-07-07 13:22:37 +0300

    ssss

--- Navigate.js
+++ Navigate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native-web";
+//import { View, Text } from "react-native-web";
 import Main from "./components/List";
 //import Form from "./components/Form";
 
