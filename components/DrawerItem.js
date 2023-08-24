a2616d212b2d15fc78a3a06f0662519b628a0d6f
cheetax <dmitriy.grebenev@gmail.com>
2023-08-24 15:08:50 +0300

    240823.1508

--- components/DrawerItem.js
+++ components/DrawerItem.js
@@ -12,23 +12,15 @@ export default function DrawerItem({ route, navigation }) {
         <DrawerContentScrollView
             style={{ backgroundColor: colors.surface }}
         >
-            <Drawer.Section >
-                <TouchableRipple
-                    onPress={toggleTheme}
-                >
-                    <View style={Styles.stackRow} >
-                        <Text variant='headlineMedium' >Темная тема</Text>
-                        <Switch value={isDarkTheme}/>
-                    </View>
-                </TouchableRipple>
-                <TouchableRipple
-                    onPress={() =>{}}
-                >
-                    <View style={Styles.stackRow} >
-                        <Text variant='headlineMedium' >О приложении</Text>
-                    </View>
-                </TouchableRipple>
-            </Drawer.Section>
+            <Drawer.Item
+                onPress={toggleTheme}
+                label="Темная тема"
+                right={(props) => <Switch value={isDarkTheme} onChange={toggleTheme} />}
+            >
+            </Drawer.Item>
+            <Drawer.Item
+                label="О приложении"
+            />
         </DrawerContentScrollView>
     )
 }
@@ -36,25 +28,25 @@ export default function DrawerItem({ route, navigation }) {
 const Styles = StyleSheet.create({
 
     main: {
-      flex: 1,
-      flexDirection: 'column',
+        flex: 1,
+        flexDirection: 'column',
     },
     forma: {
-      flex: 1,
-      flexDirection: 'column',
-      marginHorizontal: 24
+        flex: 1,
+        flexDirection: 'column',
+        marginHorizontal: 24
     },
     text: {
-      paddingHorizontal: 0,
-      paddingRight: 8,
+        paddingHorizontal: 0,
+        paddingRight: 8,
     },
     inputField: {
-      marginTop: 12,
-      height: 56
+        marginTop: 12,
+        height: 56
     },
     stackRow: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      flex: 1
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flex: 1
     }
-  })
+})
