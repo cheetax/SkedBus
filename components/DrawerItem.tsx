cd3f9b95f3a3eb7e967c415e6c6a945725254ed3
cheetax <dmitriy.grebenev@gmail.com>
2023-11-10 15:24:31 +0300

    10112023.1524

---
+++ components/DrawerItem.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from "react";
+import { StatusBar } from 'react-native';
+import { Drawer, Switch, Card } from 'react-native-paper';
+import { useAppContext } from "../providers/AppContextProvider";
+
+const DrawerItem: FC = () => {
+    const { toggleTheme, isDarkTheme } = useAppContext();
+    return (
+        <Card
+            style={{ height: '100%', paddingTop: StatusBar.currentHeight }}
+        >
+            <Drawer.Item
+                onPress={toggleTheme}
+                label="Темная тема"
+                right={(props) => <Switch value={isDarkTheme} onChange={toggleTheme} />}
+            >
+            </Drawer.Item>
+            <Drawer.Item
+                label="О приложении"
+            />
+        </Card >
+    )
+}
+export default DrawerItem
