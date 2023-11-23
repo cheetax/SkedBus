88f7da75378ff73386b486d8763f2e92083d21b1
cheetax <dmitriy.grebenev@gmail.com>
2023-11-23 16:53:39 +0300

    23112023.1653

--- components/Login.tsx
+++ components/Login.tsx
@@ -21,29 +21,48 @@ WebBrowser.maybeCompleteAuthSession();
 const FormLogin = ({ navigation }: Props) => {
 
     const { user, setUser } = useUserContext()
+    const [userInfo, setUserInfo] = useState(null)
     const [request, response, promtAsync] = Google.useAuthRequest({
-        webClientId: "784049700606-8ktkaj1t7ltm49rkucs2vmja3ovl90ak.apps.googleusercontent.com",
-        androidClientId: "784049700606-2rrj0q80uap40353e6m203mubl2m7vj2.apps.googleusercontent.com"
+        webClientId: "972891890305-2jf1bn92gqhg84nqq517otlscsrj29mu.apps.googleusercontent.com",
+        androidClientId: "972891890305-652plr3j3rnb4r8bl7eiu8pfaup5j4s3.apps.googleusercontent.com"
     })
 
     useEffect(() => {
-     //   GoogleSignin.configure()
-    }, [])
+        handleSingInWithGoogle()
+    }, [response])
 
-    const signIn = async () => {
-        setUser({name: 'Дмитрий Гребенев', email: 'dmitriy.grebenev@gmail.com', avatar: 'https://photo-pict.com/wp-content/uploads/2019/05/kartinki-dlya-stima-12.jpg'})
-        try {
-           // await GoogleSignin.hasPlayServices()
-          //  const userInfo = await GoogleSignin.signIn()
-           // console.log(userInfo)
-        } catch {
+    const handleSingInWithGoogle = async () => {
+        if (!userInfo) {
+            if (response?.type === 'success') {
+                await getUserInfo(response.authentication?.accessToken)
+            }
+        } else {
 
         }
     }
 
+    const getUserInfo = async (token: string | undefined) => {
+        setUser({ name: 'Дмитрий Гребенев', email: 'dmitriy.grebenev@gmail.com', avatar: 'https://photo-pict.com/wp-content/uploads/2019/05/kartinki-dlya-stima-12.jpg' })
+        if (!token) return
+        try {
+            const response = await fetch("https://www.googleapis.com/userinfo/v2/me",
+                {
+                    headers: { Authorization: 'Bearer ' + token }
+                })
+
+            const user = await response.json()
+            setUserInfo(user)
+            // await GoogleSignin.hasPlayServices()
+            //  const userInfo = await GoogleSignin.signIn()
+            // console.log(userInfo)
+        } catch (error) {
+
+        }
+    }
+    console.log(userInfo)
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
-            <Text>Логин форма</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
+            {/* <Text>{userInfo}</Text> */}
             <Button onPress={() => promtAsync()} >Войти </Button>
         </View>
     )
